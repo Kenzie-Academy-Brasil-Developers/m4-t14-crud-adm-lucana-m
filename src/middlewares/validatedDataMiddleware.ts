91e256e23bcd4d17c506dd1c59c441fd12755bc6
lucana-m <lucana_monica@hotmail.com>
2023-02-27 08:10:17 -0300

    Fix validation admin to create user

--- src/middlewares/validatedDataMiddleware.ts
+++ src/middlewares/validatedDataMiddleware.ts
@@ -4,7 +4,14 @@ import { ZodTypeAny } from "zod";
 export const validatedDataMiddleware =
   (schema: ZodTypeAny) =>
   (request: Request, response: Response, next: NextFunction) => {
-    const validatedData = schema.parse(request.body);
+    const verifyContainsAdmin = Object.keys(request.body).includes("admin");
+    let newRequestData = request.body;
+
+    if (!verifyContainsAdmin) {
+      newRequestData.admin = false;
+    }
+
+    const validatedData = schema.parse(newRequestData);
 
     request.body = validatedData;
 
